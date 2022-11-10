d2bca04950a6b8aec4e030a5290d9d4e1c276bfb
okayblue <56899411+okayblue@users.noreply.github.com>
2022-11-10 14:49:34 -0500

    Password validation with javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+const pass = document.querySelector(".pass");
+const pass2 = document.querySelector(".confirm");
+const errormessage = document.querySelector(".errormessage")
+const form = document.querySelector(".signup-form");
+
+pass.addEventListener('keyup', confirmPass)
+pass2.addEventListener('keyup', confirmPass)
+
+function validate() {
+  if (confirmPass() == false) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function confirmPass() {
+  if (pass.value !== pass2.value) {
+    errormessage.textContent = "* Passwords do not match";
+    pass.classList.add('error');
+    pass2.classList.add('error');   
+    return false;
+  } else if (pass.value === pass2.value) {
+    errormessage.textContent = '';
+    pass.classList.remove('error');
+    pass2.classList.remove('error');  
+    return true;
+  }
+}
+
+
